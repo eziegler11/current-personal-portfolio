f6a4a4d6f7d4e1e4515cb750bf964c96fbd238b3
erichziegler <erich.ziegler11@gmail.com>
2023-07-06 19:24:22 -0600

    Header component finished.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,25 @@
-import './header.css'
+import './header.css';
+import CTA from './CTA';
+import ME from '../../assets/me.png';
+import HeaderSocial from './HeaderSocials';
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
-export default Header
+	return (
+		<header>
+			<div className='container header__container'>
+				<h5>Hello I'm</h5>
+				<h1>Erich Ziegler</h1>
+				<h5 className='text-light'>Full-Stack Developer</h5>
+        <CTA />
+        <HeaderSocial />
+
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
+			</div>
+		</header>
+	);
+};
+export default Header;
