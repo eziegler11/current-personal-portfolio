4aa69fa8a56a6f1ade5256c3b09acd7eb78bc5ee
erichziegler <erich.ziegler11@gmail.com>
2023-07-13 19:16:47 -0600

    Footer completed.

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -45,7 +45,7 @@ const data = [
 const Testimonials = () => {
   return (
 		<section id='testimonials'>
-			<h5>Review from clients</h5>
+			<h5>Reviews From Clients</h5>
 			<h2>Testimonials</h2>
 
 			<Swiper
@@ -60,7 +60,7 @@ const Testimonials = () => {
 					return (
 						<SwiperSlide key={index} className='testimonial'>
 							<div className='client__avatar'>
-								<img src={avatar} />
+								<img src={avatar} alt='testimonial'/>
 							</div>
 							<h5 className='client__name'>{name}</h5>
 							<small className='client__review'>{review}</small>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
-
+createRoot(document.querySelector('#root')).render(<App />);
